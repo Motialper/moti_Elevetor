239b941204806a38cf3d53548723ffa54f22d185
Motialper <motialp770@gmail.com>
2024-05-30 08:59:11 +0300

    Delete src directory

--- src/factoryClassass/BuildingFactory.tsx
+++
@@ -1,9 +0,0 @@
-import { Building } from "../building/Building";
-
- class BuildingFactory {
-    static createBuilding(numFloors: number, numElevators: number): Building {
-        return new Building(numFloors, numElevators);
-    }
-}
-
-export default BuildingFactory;
